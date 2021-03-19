2012d982a92bc5a1531526864727b2d14bfa20b5
HarllanAndrye <harllanandrye@gmail.com>
2021-03-18 23:47:54 -0300

    tests

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,10 @@
 import { Sequelize } from 'sequelize';
 
 //console.log(process.env.DATABASE_URL);
-const sequelize = new Sequelize(process.env.DATABASE_URL || '');
+const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 export default sequelize;
